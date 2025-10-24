85fbd539213cbee6ac6f34a0b6a5f5c345c54416
Anushka5678661 <negianushka332@gmail.com>
2025-10-24 11:11:52 +0530

    Q8

---
+++ h8.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main ()
+{
+    int i, sum=0;
+    for(i=1; i<1000; i++)
+    {
+    if( i%3 ==0 || i%5 ==0)
+    {
+        sum += i;
+    }
+    }
+    printf("The sum of all the multiples of 3 or 5 is: %d" ,sum);
+    
+    return 0;
+}
