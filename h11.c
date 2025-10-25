e2a94657e58331e04c116f35accf749e7cfe8a79
Anushka5678661 <negianushka332@gmail.com>
2025-10-25 12:16:56 +0530

    Q11

---
+++ h11.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    if (num % 2 == 0) {
+        printf("The number %d is Even.\n", num);
+    } else {
+        printf("The number %d is Odd.\n", num);
+    }
+    return 0;
+}
